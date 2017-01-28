a2a86145fc0f585899509cc75a1fd1b52c5503a1
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-01-28 17:51:24 +0100

    feat: add methods to format release date and vote average to Movies

--- app/src/main/java/utilities/Movie.java
+++ app/src/main/java/utilities/Movie.java
@@ -3,6 +3,9 @@ package utilities;
 /**
  * Created by piero on 1/24/17.
  */
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -10,10 +13,20 @@ public class Movie {
     private static final String TAG = Movie.class.getSimpleName();
     public String posterPath, backdropPath, title, overview;
     public Date releaseDate;
+    public String vote_average;
 
     @Override
     public String toString(){
         return title + ", " + releaseDate.toString() + ", " + overview.length() + ", " +
                 posterPath.length() + ", " + backdropPath.length();
+    }
+
+    public String formatedReleaseDate() {
+        DateFormat df = new SimpleDateFormat("MMMM dd, yyyy");
+        return df.format(releaseDate);
+    }
+
+    public String formatedVoteAverage() {
+        return vote_average + " / 10";
     }
 }
