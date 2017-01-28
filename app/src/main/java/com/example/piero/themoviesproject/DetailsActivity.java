98eabce191f6ec90d8f6abe5c90b091ea801bbd4
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-01-28 17:52:43 +0100

    feat: set text for TextViews in DetailsActivity

--- app/src/main/java/com/example/piero/themoviesproject/DetailsActivity.java
+++ app/src/main/java/com/example/piero/themoviesproject/DetailsActivity.java
@@ -2,9 +2,11 @@ package com.example.piero.themoviesproject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import utilities.Movie;
 import utilities.NetworkUtils;
 
 
@@ -15,16 +17,26 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView imageView = (ImageView) findViewById(R.id.iv_thumbnail);
+        TextView mTitleTextView = (TextView) findViewById(R.id.tv_title);
+        TextView mReleaseDateTextView = (TextView) findViewById(R.id.tv_release_date);
+        TextView mVoteAverageTextView = (TextView) findViewById(R.id.tv_vote_average);
+        TextView mOverviewTextView = (TextView) findViewById(R.id.tv_overview);
 
         int position = getIntent().getIntExtra("position", -1);
+        Movie movie = ListActivity.movies[position];
         if (position != -1) {
+            mTitleTextView.setText(movie.title);
+            mReleaseDateTextView.setText(movie.formatedReleaseDate());
+            mVoteAverageTextView.setText(movie.formatedVoteAverage());
+            mOverviewTextView.setText(movie.overview);
+
             Picasso.with(DetailsActivity.this)
-                    .load(NetworkUtils.buildPosterUrl(ListActivity.movies[position]))
+                    .load(NetworkUtils.buildPosterUrl(movie))
                     //.placeholder(R.raw.place_holder)
                     .noFade()
+                    .resize(500, 0)
                     //.error(R.raw.big_problem)
                     .into(imageView);
-
         }
 
 //        } else {
