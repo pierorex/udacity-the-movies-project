90a6cddd2b84cc35e70f8b6341201086675ccf0b
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-01-29 01:11:41 +0100

    feat: handle network issues (loading and no connection)

--- app/src/main/java/com/example/piero/themoviesproject/ListActivity.java
+++ app/src/main/java/com/example/piero/themoviesproject/ListActivity.java
@@ -2,9 +2,12 @@ package com.example.piero.themoviesproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,6 +17,9 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -27,25 +33,28 @@ public class ListActivity extends AppCompatActivity {
     private static final String TAG = ListActivity.class.getSimpleName();
     public static Movie[] movies;
     String sort_order = "popular";
+    private ProgressBar mLoadingIndicator;
+    private GridView mGridView;
+    private TextView mErrorMessageDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        mGridView = (GridView) findViewById(R.id.gv_movie_list);
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
         loadMoviesData(NetworkUtils.buildPopularUrl());
     }
 
     private void loadMoviesData(URL url) {
-        //showWeatherDataView();
-
         new FetchMoviesTask().execute(url);
     }
 
     private void loadGridView() {
-        GridView gridview = (GridView) findViewById(R.id.gv_movie_list);
-        gridview.setAdapter(new ImageAdapter(this));
+        mGridView.setAdapter(new ImageAdapter(this));
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(ListActivity.this, DetailsActivity.class);
@@ -57,37 +66,56 @@ public class ListActivity extends AppCompatActivity {
 
     public class FetchMoviesTask extends AsyncTask<URL, Void, Movie[]> {
         private final String TAG = FetchMoviesTask.class.getSimpleName();
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//            //mLoadingIndicator.setVisibility(View.VISIBLE);
-//        }
+
+        public boolean isOnline() {
+            ConnectivityManager cm =
+                    (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            return netInfo != null && netInfo.isConnectedOrConnecting();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected Movie[] doInBackground(URL... params) {
-            URL requestUrl = params[0];
+            if (isOnline()) {
+                URL requestUrl = params[0];
 
-            try {
-                String jsonResponse = NetworkUtils.getResponseFromHttpUrl(requestUrl);
-                return JsonUtils.getMoviesArrayFromJson(ListActivity.this, jsonResponse);
-            } catch (Exception e) {
+                try {
+                    String jsonResponse = NetworkUtils.getResponseFromHttpUrl(requestUrl);
+                    return JsonUtils.getMoviesArrayFromJson(ListActivity.this, jsonResponse);
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+            else {
                 return null;
             }
         }
 
         @Override
         protected void onPostExecute(Movie[] fetchedMovies) {
-            //mLoadingIndicator.setVisibility(View.INVISIBLE);
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+
             if (fetchedMovies != null) {
-                //showWeatherDataView();
-                //mForecastAdapter.setWeatherData(weatherData);
                 movies = fetchedMovies;
                 loadGridView();
+                showMovies();
             }
-            //else {
-                //showErrorMessage();
-            //}
+            else {
+                Log.e(TAG, "No internet access/connection.");
+                Toast.makeText(
+                        ListActivity.this, "No internet connection",
+                        Toast.LENGTH_SHORT)
+                        .show();
+                if (movies == null) {
+                    showErrorMessage();
+                }
+            }
         }
     }
 
@@ -153,20 +181,43 @@ public class ListActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_sort_popular && sort_order.equals("top_rated")) {
-            movies = null;
+        if (id == R.id.action_sort_popular && !sort_order.equals("popular")) {
             sort_order = "popular";
             loadMoviesData(NetworkUtils.buildPopularUrl());
             return true;
         }
 
-        if (id == R.id.action_sort_top_rated && sort_order.equals("popular")) {
-            movies = null;
+        if (id == R.id.action_sort_top_rated && !sort_order.equals("top_rated")) {
             sort_order = "top_rated";
             loadMoviesData(NetworkUtils.buildTopRatedUrl());
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method will make the error message visible and hide the weather
+     * View.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showErrorMessage() {
+        sort_order = "error";
+        mGridView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the View for the movie data visible and
+     * hide the error message.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showMovies() {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mGridView.setVisibility(View.VISIBLE);
     }
 }
